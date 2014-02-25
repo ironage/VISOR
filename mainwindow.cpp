a4223808f10529af28b2e8f75550a876dde79d0f
Robert Prior <rjcprior@gmail.com>
2014-02-24 23:41:29 -0800

    Output image has the black border cropped, moved core functionality of stitching to a seperate function (which the stitchImagesClicked will theoretically call in a loop of all images)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -71,6 +71,96 @@ void MainWindow::openImageClicked() {
     inputImages.clear();
 }
 
+cv::Mat result;
+void stitchImages(Mat &image1, Mat &image2 ) {
+
+    //-- Step 1: Detect the keypoints using SURF Detector
+     int minHessian = 400;
+
+    SurfFeatureDetector detector( minHessian );
+
+    std::vector< KeyPoint > keypoints_object, keypoints_scene;
+
+    detector.detect( image1, keypoints_object );
+    detector.detect( image2, keypoints_scene );
+
+    //-- Step 2: Calculate descriptors (feature vectors)
+     SurfDescriptorExtractor extractor;
+
+    Mat descriptors_object, descriptors_scene;
+
+    extractor.compute( image1, keypoints_object, descriptors_object );
+     extractor.compute( image2, keypoints_scene, descriptors_scene );
+
+    //-- Step 3: Matching descriptor vectors using FLANN matcher
+     FlannBasedMatcher matcher;
+     std::vector< DMatch > matches;
+     matcher.match( descriptors_object, descriptors_scene, matches );
+
+    double max_dist = 0; double min_dist = 100;
+
+    //-- Quick calculation of max and min distances between keypoints
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+        if( dist > max_dist ) max_dist = dist;
+    }
+
+    printf("-- Max dist : %f \n", max_dist );
+     printf("-- Min dist : %f \n", min_dist );
+
+    //-- Use only "good" matches (i.e. whose distance is less than 3*min_dist )
+     std::vector< DMatch > good_matches;
+
+     for( int i = 0; i < descriptors_object.rows; i++ )
+     {
+         if( matches[i].distance < 3*min_dist )
+         { good_matches.push_back( matches[i]); }
+     }
+     std::vector< Point2f > obj;
+     std::vector< Point2f > scene;
+
+    for( int i = 0; i < good_matches.size(); i++ )
+     {
+     //-- Get the keypoints from the good matches
+     obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+     scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+     }
+
+    // Find the Homography Matrix
+     Mat H = findHomography( obj, scene, CV_RANSAC );
+     // Use the Homography Matrix to warp the images
+
+     warpPerspective(image1,result,H,cv::Size(image1.cols+image2.cols,image1.rows+image2.rows));
+     cv::Mat half(result,cv::Rect(0,0,image2.cols,image2.rows));
+     image2.copyTo(half);
+
+     //crop the black part of the image
+     cv::Mat mask;
+     vector<vector<Point> > contours; //no c++ 11 rabble rabble
+
+     //need grey image to remove black box
+     Mat gray_result;
+     // Convert to Grayscale
+     cvtColor( result, gray_result, CV_RGB2GRAY );
+
+     threshold(gray_result, mask, 1.0, 255.0, CHAIN_APPROX_SIMPLE);
+     findContours(mask, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+     double maxContourArea = 0.0;
+     unsigned maxContourIndex, i;
+     for (i  = 0; i < contours.size(); ++i) {
+         double a = contourArea(contours[i]);
+         if (a > maxContourArea) {
+             maxContourArea  = a;
+             maxContourIndex = i;
+         }
+     }
+
+     cv::Rect boundingBox = boundingRect(contours[maxContourIndex]);
+     result = result(boundingBox);
+}
+
 void MainWindow::stitchImagesClicked() {
 
     QStringList names = QFileDialog::getOpenFileNames();
@@ -79,79 +169,9 @@ void MainWindow::stitchImagesClicked() {
 
     Mat image1= imread( names.at(0).toStdString() );
     Mat image2= imread( names.at(1).toStdString() );
-    Mat gray_image1;
-    Mat gray_image2;
-    // Convert to Grayscale
-    cvtColor( image1, gray_image1, CV_RGB2GRAY );
-    cvtColor( image2, gray_image2, CV_RGB2GRAY );
 
-    imshow("first image",image2);
-    imshow("second image",image1);
 
-   if( !gray_image1.data || !gray_image2.data )
-    { std::cout<< " --(!) Error reading images " << std::endl; return; }
-
-   //-- Step 1: Detect the keypoints using SURF Detector
-    int minHessian = 400;
-
-   SurfFeatureDetector detector( minHessian );
-
-   std::vector< KeyPoint > keypoints_object, keypoints_scene;
-
-   detector.detect( gray_image1, keypoints_object );
-    detector.detect( gray_image2, keypoints_scene );
-
-   //-- Step 2: Calculate descriptors (feature vectors)
-    SurfDescriptorExtractor extractor;
-
-   Mat descriptors_object, descriptors_scene;
-
-   extractor.compute( gray_image1, keypoints_object, descriptors_object );
-    extractor.compute( gray_image2, keypoints_scene, descriptors_scene );
-
-   //-- Step 3: Matching descriptor vectors using FLANN matcher
-    FlannBasedMatcher matcher;
-    std::vector< DMatch > matches;
-    matcher.match( descriptors_object, descriptors_scene, matches );
-
-   double max_dist = 0; double min_dist = 100;
-
-   //-- Quick calculation of max and min distances between keypoints
-   for( int i = 0; i < descriptors_object.rows; i++ )
-   { double dist = matches[i].distance;
-       if( dist < min_dist ) min_dist = dist;
-       if( dist > max_dist ) max_dist = dist;
-   }
-
-   printf("-- Max dist : %f \n", max_dist );
-    printf("-- Min dist : %f \n", min_dist );
-
-   //-- Use only "good" matches (i.e. whose distance is less than 3*min_dist )
-    std::vector< DMatch > good_matches;
-
-    for( int i = 0; i < descriptors_object.rows; i++ )
-    {
-        if( matches[i].distance < 3*min_dist )
-        { good_matches.push_back( matches[i]); }
-    }
-    std::vector< Point2f > obj;
-    std::vector< Point2f > scene;
-
-   for( int i = 0; i < good_matches.size(); i++ )
-    {
-    //-- Get the keypoints from the good matches
-    obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
-    scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
-    }
-
-   // Find the Homography Matrix
-    Mat H = findHomography( obj, scene, CV_RANSAC );
-    // Use the Homography Matrix to warp the images
-    cv::Mat result;
-    warpPerspective(image1,result,H,cv::Size(image1.cols+image2.cols,image1.rows+image2.rows));
-    cv::Mat half(result,cv::Rect(0,0,image2.cols,image2.rows));
-    image2.copyTo(half);
-    imshow( "Result", result );
+    stitchImages(image1, image2 );
 
 
     cvtColor(result, result,CV_BGR2RGB);
