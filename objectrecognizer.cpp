4582148bf31689c4222024757b4feb397f17a9d6
Jason Deglint <jdeglint@ubuntu.(none)>
2014-03-11 13:05:47 -0700

    now have basic shape detection functionality

--- objectrecognizer.cpp
+++ objectrecognizer.cpp
@@ -35,59 +35,61 @@ Mat ObjectRecognizer::recognizeObjects() {
     inputImage.copyTo(cannyImage, bwImage);  // bwImage is our mask
     //saveImage(cannyImage, "03_cannyImage.jpg");
 
-    /// output image
-    cv::Mat outputImage;
-    cv::Mat binaryImage(imageSize, CV_8UC1);
-    inputImage.copyTo(outputImage);
+    /// Hough Transform
+    Mat lineImage(imageSize, CV_8UC3, Scalar(255,255,255));
     vector<Vec4i> lines;
     HoughLinesP(bwImage, lines, 1, CV_PI/180, houghVote, houghMinLength, houghMinDistance);
     for( size_t i = 0; i < lines.size(); i++ )
     {
       Vec4i l = lines[i];
-      line( outputImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
-      //line( binaryImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+      line( lineImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,0), 3, CV_AA);
     }
     ///saveImage(outputImage, "04_outputImage.jpg");
 
-    /*
-    // Find Contours
+
+    /// Find Contours
+    cv::Mat contourGrayImage, contourBwImage;                           // prehaps don't need this line
+    cv::cvtColor(lineImage, contourGrayImage, CV_BGR2GRAY);             // prehaps don't need this line
+    cv::Canny(contourGrayImage, contourBwImage,cannyLow, cannyHigh);   // prehaps don't need this line
     std::vector<std::vector<cv::Point> > contours;
-    cv::findContours(bwImage.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-    std::cout << "I'm here..." << std::endl;
+    cv::findContours(contourBwImage.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-    // approximation polygons
+    cv::Mat cannyImage2;
+    inputImage.copyTo(cannyImage2, contourBwImage);
+
+    /// Approximation Polygons
+    cv::Mat outputImage;
+    inputImage.copyTo(outputImage);
     std::vector<cv::Point> approxPolygon;
-    cv::Mat outputImage = inputImage;                   // put label is destination image
     for(int i=0; i<contours.size(); i++){
-        cv::approxPolyDP(cv::Mat(contours[i]), approxPolygon, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
-        //cv::approxPolyDP(cv::Mat(contours[i]), approxPolygon, cv::arcLength(cv::Mat(contours[i]), false)*0.00002, false);
+        cv::approxPolyDP(cv::Mat(contours[i]), approxPolygon, cv::arcLength(cv::Mat(contours[i]), true)*0.01, true);
 
-        // skip small of non-convex objects
+        // skip small or non-convex objects
         if(std::fabs(cv::contourArea(contours[i]))<100 || !cv::isContourConvex(approxPolygon)){
             continue;
         }
         if(approxPolygon.size() == 3){
-            setLabel(outputImage, "TRIANGLE", contours[i]);
-            drawPolygon(outputImage, approxPolygon);
+            SharedFunctions::setLabel(outputImage, "TRIANGLE", contours[i]);
+            SharedFunctions::drawPolygon(outputImage, approxPolygon);
         }
         else if(approxPolygon.size() == 4){
-            setLabel(outputImage, "SQUARE", contours[i]);
-            drawPolygon(outputImage, approxPolygon);
+            SharedFunctions::setLabel(outputImage, "SQUARE", contours[i]);
+            SharedFunctions::drawPolygon(outputImage, approxPolygon);
         }
         else if(approxPolygon.size() == 5){
-            setLabel(outputImage, "PENTAGON", contours[i]);
-            drawPolygon(outputImage, approxPolygon);
+            SharedFunctions::setLabel(outputImage, "PENTAGON", contours[i]);
+            SharedFunctions::drawPolygon(outputImage, approxPolygon);
         }
         else if(approxPolygon.size() == 6){
-            setLabel(outputImage, "HEXAGON", contours[i]);
-            drawPolygon(outputImage, approxPolygon);
+            SharedFunctions::setLabel(outputImage, "HEXAGON", contours[i]);
+            SharedFunctions::drawPolygon(outputImage, approxPolygon);
         }
         else if(approxPolygon.size() > 15){
-            setLabel(outputImage, "CIRCLE", contours[i]);
-            drawPolygon(outputImage, approxPolygon);
+            SharedFunctions::setLabel(outputImage, "CIRCLE", contours[i]);
+            SharedFunctions::drawPolygon(outputImage, approxPolygon);
         }
     }
-    saveImage(outputImage, "outputImage.jpg");
-    */
-    return outputImage;
+    //saveImage(outputImage, "outputImage.jpg");
+
+    return lineImage;
 }
